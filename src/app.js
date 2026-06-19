2b3b2296d37b07046f14d7556b90346a0c27718e
Vaibhav Garg <VaibhavGarg259@gmail.com>
2026-06-19 15:49:08 +0530

    add user and video models

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+app.use(express.json({ limit: "16kb" })); //json access but limit
+app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url me kuch specision dota like #* access
+app.use(express.static("public")); //file or folder is store
+app.use(cookieParser());//borwer ka data ko access kr or store
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@ import mongoose from "mongoose";
 import connectDB from "../db/connectDB.js";
 import express from "express";
 
+const app = express();
 dotenv.config({
   path: "./.env",
 });
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port:${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!!", err);
+  });
 
-connectDB();
 
+
+
+
+
+
+  
 // import express from "express";
 // const app = express();
 // (async () => {
